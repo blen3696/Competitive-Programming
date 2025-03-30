0191c5f53dd14064a1891aa41eadb7e62a5b96f3
Blen Redwan <163293320+blen3696@users.noreply.github.com>
2025-03-30 20:53:40 +0300

    Time: 313 ms (14.62%), Space: 20.5 MB (85.94%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        def validator(m):
+            i, j = 0, 0
+            while i < len(houses) and j < len(heaters):
+                if abs(houses[i] - heaters[j]) <= m:
+                    i += 1  
+                else:
+                    j += 1  # 
+            return i == len(houses)  
+        
+        houses.sort()
+        heaters.sort()
+        l, r = 0, max(max(houses) - min(heaters), max(heaters) - min(houses))
+        
+        while l <= r:
+            m = (l + r) // 2
+            if validator(m):
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return l
+
